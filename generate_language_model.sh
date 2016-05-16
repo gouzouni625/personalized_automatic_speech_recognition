a9a16e8cfff8c71f611420730c435172a71856db
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-05-16 18:00:21 +0300

    Add generate language model script

---
+++ generate_language_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH=$PWD/cmuclmtk-0.7/installation/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/cmuclmtk-0.7/installation/lib:$LD_LIBRARY_PATH
+
+TEXTFILE=$1
+OUTPUT_FILE=$2
+N=$3
+
+cat $TEXTFILE | cmuclmtk-0.7/text2wfreq > $TEXTFILE.freq
+
+cat $TEXTFILE.freq | wfreq2vocab > $TEXTFILE.vocab
+
+cat $TEXTFILE | text2idngram -n $N -vocab $TEXTFILE.vocab -idngram $TEXTFILE.idngram -write_ascii
+
+idngram2lm -n $N -idngram $TEXTFILE.idngram -vocab $TEXTFILE.vocab -arpa $OUTPUT_FILE -ascii_input 
