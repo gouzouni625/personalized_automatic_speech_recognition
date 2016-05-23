1dadbf0f10191400894abffa271427030b09ae28
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-05-23 11:34:57 +0300

    Initialize recognizer inside secondary thread

--- core/src/main/java/org/pasr/gui/controllers/ASRSceneController.java
+++ core/src/main/java/org/pasr/gui/controllers/ASRSceneController.java
@@ -10,6 +10,8 @@ import org.pasr.corpus.Corpus;
 import org.pasr.postp.dictionary.Dictionary;
 import org.pasr.postp.engine.Corrector;
 
+import java.io.IOException;
+
 
 public class ASRSceneController {
     @FXML
@@ -18,26 +20,31 @@ public class ASRSceneController {
     @FXML
     private TextArea asrCorrected;
 
-    public ASRSceneController(Corpus corpus, Dictionary dictionary) throws Exception {
-        Configuration configuration = new Configuration();
-
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration.setLanguageModelPath("cmuclmtk-0.7/language_model.lm");
-
-        recognizer_ = new LiveSpeechRecognizer(configuration);
-        // recognizer_.loadTransform("acoustic_model_adaptation/mllr_matrix", 1);
-
+    public ASRSceneController(Corpus corpus, Dictionary dictionary) {
         corrector_ = new Corrector(corpus, dictionary);
     }
 
     @FXML
     public void initialize(){
-        recognizerRunning_ = true;
-
-        recognizer_.startRecognition(true);
-
         new Thread(() -> {
+            Configuration configuration = new Configuration();
+
+            configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+            configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+            configuration.setLanguageModelPath("cmuclmtk-0.7/language_model.lm");
+
+            try {
+                recognizer_ = new LiveSpeechRecognizer(configuration);
+                // recognizer_.loadTransform("acoustic_model_adaptation/mllr_matrix", 1);
+
+                recognizerRunning_ = true;
+
+                recognizer_.startRecognition(true);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             SpeechResult result;
             while ((result = recognizer_.getResult()) != null && recognizerRunning_) {
                 String hypothesis = result.getHypothesis();
