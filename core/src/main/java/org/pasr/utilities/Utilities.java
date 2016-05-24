89237575311eaa5f3fd97a6826b8f659ceec4369
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-05-24 14:09:27 +0300

    Remove resource word in resources paths

--- core/src/main/java/org/pasr/utilities/Utilities.java
+++ core/src/main/java/org/pasr/utilities/Utilities.java
@@ -6,6 +6,10 @@ import java.io.InputStream;
 
 public class Utilities {
     public static InputStream getResourceStream(String resource){
+        if(resource.substring(0, 9).equals("resource:")){
+            resource = resource.substring(9);
+        }
+
         return Utilities.class.getResourceAsStream(resource);
     }
 
