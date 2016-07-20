8bba8c825d32dd82e4568f855e47642f2f9d3f8a
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-07-20 20:51:40 +0300

    Remove sphinx4 submodule

--- quality/src/main/java/org/metrics/WordErrorRate.java
+++ quality/src/main/java/org/metrics/WordErrorRate.java
@@ -4,11 +4,14 @@ package org.metrics;
 import edu.cmu.sphinx.api.SpeechResult;
 import org.configuration.Configuration;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
-import org.pasr.corpus.Corpus;
-import org.pasr.corpus.WordSequence;
-import org.pasr.postp.dictionary.Dictionary;
-import org.utilities.LevenshteinMatrix;
+import org.pasr.prep.corpus.Corpus;
+import org.pasr.prep.corpus.WordSequence;
+import org.pasr.asr.dictionary.Dictionary;
+import org.pasr.postp.engine.Corrector;
+import org.pasr.postp.engine.algorithms.PhoneDistanceAlgorithm;
+import org.pasr.utilities.LevenshteinMatrix;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -19,8 +22,12 @@ import static org.pasr.utilities.Utilities.getResourceStream;
 public class WordErrorRate{
     public WordErrorRate(Configuration configuration) throws IOException {
         corpus_ = Corpus.createFromStream(getResourceStream(configuration.getCorpusPath()));
+        corpus_.process(null);
         dictionary_ = Dictionary.createFromStream(getResourceStream(configuration.getDictionaryPath()));
         samples_ = configuration.getSamples();
+
+        corrector_ = new Corrector(corpus_, dictionary_, null);
+        corrector_.addCorrectionAlgorithm(new PhoneDistanceAlgorithm());
 
         edu.cmu.sphinx.api.Configuration sphinxConfiguration = new edu.cmu.sphinx.api.Configuration();
         sphinxConfiguration.setAcousticModelPath(configuration.getAcousticModelPath());
@@ -32,6 +39,8 @@ public class WordErrorRate{
     private Corpus corpus_;
     private Dictionary dictionary_;
 
+    private Corrector corrector_;
+
     private List<String> samples_;
 
     private StreamSpeechRecognizer recognizer_;
@@ -41,12 +50,14 @@ public class WordErrorRate{
     private int numberOfWordEdits = 0;
     private int numberOfReferenceWords = 0;
 
-    public void start(){
+    public void start() throws FileNotFoundException {
         isReady_ = false;
 
         Iterator<WordSequence> corpusIterator = corpus_.iterator();
 
         for(String sample : samples_){
+            System.out.println("Processing sample: " + sample);
+
             recognizer_.startRecognition(getResourceStream(sample));
             SpeechResult recognitionResult;
             StringBuilder hypothesisBuilder = new StringBuilder();
@@ -57,7 +68,9 @@ public class WordErrorRate{
 
             recognizer_.stopRecognition();
 
-            updateErrorWordRate(corpusIterator.next().toString(), hypothesisBuilder.toString());
+            // updateErrorWordRate(corpusIterator.next().toString(), correctionBuilder.toString());
+            // updateErrorWordRate(corpusIterator.next().toString(), hypothesisBuilder.toString());
+            updateErrorWordRate(corpusIterator.next().toString(), corrector_.correct(hypothesisBuilder.toString()));
         }
 
 
@@ -69,7 +82,7 @@ public class WordErrorRate{
     }
 
     public double getResult(){
-        return ((double)(numberOfWordEdits)) / ((double)(numberOfReferenceWords));
+        return ((double)(numberOfWordEdits)) / ((double)(numberOfReferenceWords)) * 100;
     }
 
     private void updateErrorWordRate(String reference, String hypothesis){
@@ -77,7 +90,19 @@ public class WordErrorRate{
         Integer[] hypothesisHashes = generateWordHashes(hypothesis);
 
         numberOfReferenceWords += referenceHashes.length;
-        numberOfWordEdits += new LevenshteinMatrix<>(hypothesisHashes, referenceHashes).getDistance();
+
+        int currentNumberOfWordEdits = new LevenshteinMatrix<>(hypothesisHashes, referenceHashes).getDistance();
+        numberOfWordEdits += currentNumberOfWordEdits;
+
+        System.out.println("Number of words: " + referenceHashes.length);
+        System.out.println("Number of edits: " + currentNumberOfWordEdits);
+
+        if(referenceHashes.length != 0) {
+            System.out.println("Accuracy: " + ((1 - ((double)(currentNumberOfWordEdits) / (double)(referenceHashes.length))) * 100) + "%");
+        }
+        else{
+            System.out.println("Accuracy: 100%");
+        }
     }
 
     private Integer[] generateWordHashes(String string){
