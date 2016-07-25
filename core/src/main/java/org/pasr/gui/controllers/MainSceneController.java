824c72fe162c07733d97ecb5cd0dcba1545bc876
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-07-25 16:17:34 +0300

    Add on click listeners for buttons in main scene

--- core/src/main/java/org/pasr/gui/controllers/MainSceneController.java
+++ core/src/main/java/org/pasr/gui/controllers/MainSceneController.java
@@ -48,6 +48,39 @@ public class MainSceneController {
                 new Tooltip(DictateButtonMessages.ENABLED.getMessage())
             );
         }
+
+        newCorpusButton.setTooltip(new Tooltip(NewCorpusButtonMessages.TOOLTIP.getMessage()));
+    }
+
+    @FXML
+    public void newCorpusButtonClicked(){
+        String emailAddressText = emailAddress.getText();
+        String passwordText = password.getText();
+
+        if(emailAddressText.isEmpty() && passwordText.isEmpty()){
+            newCorpusButtonLabel.setVisible(true);
+            newCorpusButtonLabel.setText(NewCorpusButtonMessages.NO_INPUT.getMessage());
+        }
+        else if(emailAddressText.isEmpty()){
+            newCorpusButtonLabel.setVisible(true);
+            newCorpusButtonLabel.setText(NewCorpusButtonMessages.NO_EMAIL_ADDRESS.getMessage());
+        }
+        else if(passwordText.isEmpty()){
+            newCorpusButtonLabel.setVisible(true);
+            newCorpusButtonLabel.setText(NewCorpusButtonMessages.NO_PASSWORD.getMessage());
+        }
+        else{
+            if(newCorpusButtonLabel.isVisible()){
+                newCorpusButtonLabel.setVisible(false);
+            }
+
+            // TODO move on with e-mail listing
+        }
+    }
+
+    @FXML
+    public void dictateButtonClicked(){
+        // TODO move on with dictation
     }
 
     @FXML
@@ -66,13 +99,34 @@ public class MainSceneController {
     private Button newCorpusButton;
 
     @FXML
+    private Label newCorpusButtonLabel;
+
+    private enum NewCorpusButtonMessages{
+        TOOLTIP("Enter the address and the password of your e-mail to fetch your e-mails" +
+            "and create a corpus"),
+        NO_INPUT("Please enter the address and the password\nof your e-mail"),
+        NO_EMAIL_ADDRESS("Please enter the address of your e-mail"),
+        NO_PASSWORD("Please enter the password of your e-mail");
+
+        NewCorpusButtonMessages(String message){
+            message_ = message;
+        }
+
+        public String getMessage(){
+            return message_;
+        }
+
+        private String message_;
+    }
+
+    @FXML
     private Button dictateButton;
 
     @FXML
     private Label dictateButtonLabel;
 
     private enum DictateButtonMessages{
-        DISABLED("Note: You should create at least one corpus\n " +
+        DISABLED("Note: You should create at least one corpus\n" +
             "and one acoustic model in order to dictate"),
         ENABLED("Use the chosen corpus and acoustic model during speech recognition");
 
