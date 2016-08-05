020036b35e71a46e908fb952811fe321648e08a6
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-08-05 18:49:49 +0300

    Add dictateToggleButton graphic listener

--- core/src/main/java/org/pasr/gui/controllers/scene/DictateController.java
+++ core/src/main/java/org/pasr/gui/controllers/scene/DictateController.java
@@ -3,11 +3,16 @@ package org.pasr.gui.controllers.scene;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import org.pasr.gui.corpus.CorpusView;
+
+import static org.pasr.utilities.Utilities.getResourceStream;
 
 
 public class DictateController extends Controller{
@@ -18,6 +23,13 @@ public class DictateController extends Controller{
     @FXML
     public void initialize(){
         corpusView.selectCorpus(((API) api_).getCorpusID());
+
+        dictateToggleButton.setGraphic(dictateToggleButtonDefaultGraphic);
+        dictateToggleButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            dictateToggleButton.setGraphic(
+                newValue ? dictateToggleButtonSelectedGraphic : dictateToggleButtonDefaultGraphic
+            );
+        });
 
         backButton.setOnAction(this :: backButtonOnAction);
     }
@@ -44,7 +56,11 @@ public class DictateController extends Controller{
     private TextArea correctedTextArea;
 
     @FXML
-    private ToggleButton dictateButton;
+    private ToggleButton dictateToggleButton;
+    private static final Node dictateToggleButtonDefaultGraphic = new ImageView(
+        new Image(getResourceStream("/icons/microphone_black.png")));
+    private static final Node dictateToggleButtonSelectedGraphic = new ImageView(
+        new Image(getResourceStream("/icons/microphone_green.png")));
 
     @FXML
     private Button backButton;
