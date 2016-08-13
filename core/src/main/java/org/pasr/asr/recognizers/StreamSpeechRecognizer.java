7382a75ae7ca71434a50952ad7c194798b7e0c77
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-08-14 00:08:06 +0300

    Refactor StreamSpeechRecognizer

--- core/src/main/java/org/pasr/asr/recognizers/StreamSpeechRecognizer.java
+++ core/src/main/java/org/pasr/asr/recognizers/StreamSpeechRecognizer.java
@@ -10,113 +10,160 @@ import org.pasr.prep.recorder.Recorder;
 import javax.sound.sampled.LineUnavailableException;
 import java.io.IOException;
 import java.util.Observable;
+import java.util.logging.Logger;
 
 
-public class StreamSpeechRecognizer extends Observable{
-    static {
-        try {
-            NativeUtils.loadLibraryFromJar("/libpocketsphinx_jni.so");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class StreamSpeechRecognizer extends Observable implements Runnable {
+    public StreamSpeechRecognizer(Configuration configuration)
+        throws IOException, LineUnavailableException {
 
-    public StreamSpeechRecognizer(Configuration configuration) throws IOException {
-        try {
-            recorder_ = new Recorder();
-        }
-        catch (LineUnavailableException e) {
-            throw new IOException("Failed to initialize recorder. Microphone might be unavailable");
-        }
+        NativeUtils.loadLibraryFromJar("/libpocketsphinx_jni.so");
+
+        recorder_ = new Recorder();
+        sampleRate_ = recorder_.getSampleRate();
 
         Config decoderConfig = Decoder.defaultConfig();
         decoderConfig.setString("-hmm", configuration.getAcousticModelPath());
         decoderConfig.setString("-dict", configuration.getDictionaryPath());
         decoderConfig.setString("-lm", configuration.getLanguageModelPath());
-
         decoder_ = new Decoder(decoderConfig);
+
+        thread_ = new Thread(this);
+        thread_.setDaemon(true);
     }
 
-    public void startRecognition(){
-        recognizingThread_ = new RecognizingThread();
-        recognizingThread_.start();
-    }
+    public synchronized void startRecognition(){
+        if(!thread_.isAlive()){
+            // Start the thread
+            thread_.start();
 
-    public void stopRecognition(){
-        if(recognizingThread_ == null){
-            return;
+            // Wait for the thread to get ready
+            while(!ready_){
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    logger_.info("Woke up while waiting for thread to be ready.");
+                }
+            }
+
+            live_ = true;
         }
 
-        try {
-            recognizingThread_.terminate();
-            recognizingThread_.join();
-        } catch (InterruptedException e) {
-            // Restore the interrupted status
-            Thread.currentThread().interrupt();
-        }
-        recognizingThread_ = null;
+        run_ = true;
+
+        // Notify the thread to wake up
+        notify();
+
+        recorder_.startRecording();
+        startDecoder();
     }
 
-    public void terminate() throws IOException {
-        stopRecognition();
+    public synchronized void stopRecognition(){
+        run_ = false;
 
-        recorder_.terminate();
+        stopDecoder();
+        recorder_.stopRecording();
     }
 
-    private class RecognizingThread extends Thread {
-        RecognizingThread (){
-            sampleRate_ = recorder_.getSampleRate();
-            bufferSize_ = Math.round(sampleRate_ * BUFFER_SIZE_SECONDS);
+    @Override
+    public void run(){
+        synchronized (this){
+            ready_ = true;
+            notify();
+
+            while(!live_){
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    logger_.info("Thread woke up while waiting to become alive.");
+                }
+            }
         }
 
-        @Override
-        public void run(){
-            recorder_.startRecording();
+        short[] buffer = new short[sampleRate_ / 10];
 
-            decoder_.startUtt();
-            short[] buffer = new short[bufferSize_];
+        while(live_){
 
             String previousHypothesis = "";
             while(run_){
-                int read = recorder_.read(buffer);
+                int readSize = recorder_.read(buffer);
 
-                if(read == -1){
-                    throw new RuntimeException("Error reading audio buffer");
-                }
-                else{
-                    decoder_.processRaw(buffer, read, false, false);
+                if(readSize > 0){
+                    decoder_.processRaw(buffer, readSize, false, false);
 
                     if(decoder_.hyp() != null){
                         String currentHypothesis = decoder_.hyp().getHypstr();
 
                         if(!currentHypothesis.equals(previousHypothesis)){
                             setChanged();
-                            notifyObservers(decoder_.hyp());
+                            notifyObservers(currentHypothesis);
 
                             previousHypothesis = currentHypothesis;
                         }
                     }
                 }
             }
-            decoder_.endUtt();
 
-            recorder_.stopRecording();
+            if(!live_){
+                break;
+            }
+
+            synchronized (this) {
+                while(!run_){
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        logger_.info("Thread woke up while waiting to run.");
+                    }
+                }
+            }
         }
-
-        public void terminate(){
-            run_ = false;
-        }
-
-        private int sampleRate_;
-        private int bufferSize_;
-        private static final float BUFFER_SIZE_SECONDS = 0.4f;
-
-        private boolean run_ = true;
     }
 
-    private Recorder recorder_;
-    private Decoder decoder_;
+    private void startDecoder(){
+        if(!isDecoderStarted_){
+            decoder_.startUtt();
+            isDecoderStarted_ = true;
+        }
+    }
 
-    private RecognizingThread recognizingThread_;
+    private void stopDecoder(){
+        if(isDecoderStarted_){
+            decoder_.endUtt();
+            isDecoderStarted_ = false;
+        }
+    }
+
+    public synchronized void terminate() {
+        run_ = false;
+        live_ = false;
+
+        notify();
+
+        try {
+            // Don't wait forever on this thread since it is a daemon and will not block the JVM
+            // from shutting down
+            thread_.join(3000);
+        } catch (InterruptedException e) {
+            logger_.warning("Interrupted while joining thread.");
+        }
+
+        stopDecoder();
+        recorder_.stopRecording();
+    }
+
+    private Thread thread_;
+
+    private volatile boolean ready_ = false;
+    private volatile boolean live_ = false;
+    private volatile boolean run_ = false;
+
+    private Recorder recorder_;
+    private final int sampleRate_;
+
+    private Decoder decoder_;
+    private boolean isDecoderStarted_ = false;
+
+    private final Logger logger_ = Logger.getLogger(getClass().getName());
 
 }
