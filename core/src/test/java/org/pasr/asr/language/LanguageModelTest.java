1fc2efdcb59d2fc21a517abdefb65973007ab122
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-08-18 21:41:40 +0300

    Refactor language model tests

--- core/src/test/java/org/pasr/asr/language/LanguageModelTest.java
+++ core/src/test/java/org/pasr/asr/language/LanguageModelTest.java
@@ -17,74 +17,74 @@ public class LanguageModelTest {
     @Before
     public void setUp() throws FileNotFoundException {
         languageModel_ = LanguageModel.createFromInputStream(getResourceStream(
-            "resource:/language_models/language_model.lm"
+            "/language_models/language_model.lm"
         ));
     }
 
     @Test
     public void testCreateFromInputStream(){
-        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word1", " ")), 1e-04);
-        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word2", " ")), 1e-04);
-        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word3", " ")), 1e-04);
-        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word4", " ")), 1e-04);
-        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word5", " ")), 1e-04);
-        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word6", " ")), 1e-04);
-        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word7", " ")), 1e-04);
-        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word8", " ")), 1e-04);
-        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word9", " ")), 1e-04);
-        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word10", " ")), 1e-04);
-        assertEquals(0.06, languageModel_.getProbability(new WordSequence("word11", " ")), 1e-04);
-        assertEquals(0.08, languageModel_.getProbability(new WordSequence("word12", " ")), 1e-04);
-        assertEquals(0.01, languageModel_.getProbability(new WordSequence("word13", " ")), 1e-04);
-        assertEquals(0.025, languageModel_.getProbability(new WordSequence("word14", " ")), 1e-04);
-        assertEquals(0.075, languageModel_.getProbability(new WordSequence("word15", " ")), 1e-04);
+        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word1")), 1e-04);
+        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word2")), 1e-04);
+        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word3")), 1e-04);
+        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word4")), 1e-04);
+        assertEquals(0.1, languageModel_.getProbability(new WordSequence("word5")), 1e-04);
+        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word6")), 1e-04);
+        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word7")), 1e-04);
+        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word8")), 1e-04);
+        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word9")), 1e-04);
+        assertEquals(0.05, languageModel_.getProbability(new WordSequence("word10")), 1e-04);
+        assertEquals(0.06, languageModel_.getProbability(new WordSequence("word11")), 1e-04);
+        assertEquals(0.08, languageModel_.getProbability(new WordSequence("word12")), 1e-04);
+        assertEquals(0.01, languageModel_.getProbability(new WordSequence("word13")), 1e-04);
+        assertEquals(0.025, languageModel_.getProbability(new WordSequence("word14")), 1e-04);
+        assertEquals(0.075, languageModel_.getProbability(new WordSequence("word15")), 1e-04);
 
-        assertEquals(0.44, languageModel_.getProbability(new WordSequence("word1 word2", " ")),
+        assertEquals(0.44, languageModel_.getProbability(new WordSequence("word1 word2")),
             1e-04);
-        assertEquals(0.09, languageModel_.getProbability(new WordSequence("word1 word5", " ")),
+        assertEquals(0.09, languageModel_.getProbability(new WordSequence("word1 word5")),
             1e-04);
-        assertEquals(0.12, languageModel_.getProbability(new WordSequence("word3 word4", " ")),
+        assertEquals(0.12, languageModel_.getProbability(new WordSequence("word3 word4")),
             1e-04);
-        assertEquals(0.24, languageModel_.getProbability(new WordSequence("word5 word6", " ")),
+        assertEquals(0.24, languageModel_.getProbability(new WordSequence("word5 word6")),
             1e-04);
-        assertEquals(0.34, languageModel_.getProbability(new WordSequence("word7 word8", " ")),
+        assertEquals(0.34, languageModel_.getProbability(new WordSequence("word7 word8")),
             1e-04);
-        assertEquals(0.55, languageModel_.getProbability(new WordSequence("word12 word14", " ")),
+        assertEquals(0.55, languageModel_.getProbability(new WordSequence("word12 word14")),
             1e-04);
-        assertEquals(0.62, languageModel_.getProbability(new WordSequence("word9 word10", " ")),
+        assertEquals(0.62, languageModel_.getProbability(new WordSequence("word9 word10")),
             1e-04);
-        assertEquals(0.43, languageModel_.getProbability(new WordSequence("word3 word13", " ")),
+        assertEquals(0.43, languageModel_.getProbability(new WordSequence("word3 word13")),
             1e-04);
-        assertEquals(0.03, languageModel_.getProbability(new WordSequence("word11 word12", " ")),
+        assertEquals(0.03, languageModel_.getProbability(new WordSequence("word11 word12")),
             1e-04);
-        assertEquals(0.11, languageModel_.getProbability(new WordSequence("word13 word15", " ")),
+        assertEquals(0.11, languageModel_.getProbability(new WordSequence("word13 word15")),
             1e-04);
 
         assertEquals(0.2000, languageModel_.getProbability(new WordSequence(
-            "word1 word2 word3", " ")), 1e-04);
+            "word1 word2 word3")), 1e-04);
         assertEquals(0.4300, languageModel_.getProbability(new WordSequence(
-            "word7 word12 word15", " ")), 1e-04);
+            "word7 word12 word15")), 1e-04);
         assertEquals(0.1200, languageModel_.getProbability(new WordSequence(
-            "word8 word9 word4", " ")), 1e-04);
+            "word8 word9 word4")), 1e-04);
         assertEquals(0.5400, languageModel_.getProbability(new WordSequence(
-            "word15 word14 word13", " ")), 1e-04);
+            "word15 word14 word13")), 1e-04);
         assertEquals(0.4000, languageModel_.getProbability(new WordSequence(
-            "word3 word5 word6", " ")), 1e-04);
+            "word3 word5 word6")), 1e-04);
 
         // A 3-gram where the first 2-gram (word2 word3) doesn't exist in the language model.
         assertEquals(0.12, languageModel_.getProbability(new WordSequence(
-            "word2 word3 word4", " ")), 1e-04);
+            "word2 word3 word4")), 1e-04);
 
         // A 3-gram where the first 2-gram (word1 word2) exists in the language model.
         assertEquals(0.3168, languageModel_.getProbability(new WordSequence(
-            "word1 word2 word4", " ")), 1e-04);
+            "word1 word2 word4")), 1e-04);
 
         // A sequence of 4 words
         assertEquals(0.001056, languageModel_.getProbability(new WordSequence(
-            "word1 word2 word3 word4", " ")), 1e-06);
+            "word1 word2 word3 word4")), 1e-06);
 
         // A sequence of 5 words
         assertEquals(0, languageModel_.getProbability(new WordSequence(
-            "word10 word11 word12 word13 word14 word15", " ")), 1e-06);
+            "word10 word11 word12 word13 word14 word15")), 1e-06);
     }
 }
