4ee81332b6c527719ef07670f3ba8e170f675a6d
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-08-19 17:20:14 +0300

    Add exit code handling in setup script

--- setup.sh
+++ setup.sh
@@ -10,13 +10,17 @@ git submodule update
 cd sphinxbase
 
 # autogen script will run configure with the given arguments
-./autogen.sh --prefix=$PWD/installation
+./autogen.sh --prefix=${PWD}/installation && make && make check && make install
 
-make
-make check
-make install
+if [ $? -eq 1 ]
+then
+  echo "Could not build sphinxbase"
+  exit 1
+else
+  echo "sphinxbase was successfully built and installed" >> setup.log
+fi
 
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$PWD
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PWD}
 
 cd ..
 
@@ -24,11 +28,15 @@ cd ..
 cd sphinxtrain
 
 # autogen script will run configure with the given arguments
-./autogen.sh --prefix=$PWD/installation
+./autogen.sh --prefix=${PWD}/installation && make && make check && make install
 
-make
-make check
-make install
+if [ $? -eq 1 ]
+then
+  echo "Could not build sphinxtrain"
+  exit 1
+else
+  echo "sphinxtrain was successfully built and installed" >> setup.log
+fi
 
 cd ..
 
@@ -36,11 +44,15 @@ cd ..
 cd pocketsphinx
 
 # autogen script will run configure with the given arguments
-./autogen.sh  --prefix=$PWD/installation
+./autogen.sh  --prefix=${PWD}/installation && make && make check && make install
 
-make
-make check
-make install
+if [ $? -eq 1 ]
+then
+  echo "Could not build pocketsphinx"
+  exit 1
+else
+  echo "pocketsphinx was successfully built and installed" >> setup.log
+fi
 
 # If no first argument is supplied
 if [ -z "$1" ]
@@ -50,12 +62,19 @@ else
     export JAVA_HOME=$1
 fi
 
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$PWD
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PWD}
 
 cd swig/java
-make
-javac edu/cmu/pocketsphinx/*.java
-jar -cf pocketsphinx.jar edu libpocketsphinx_jni.so
+
+make && javac edu/cmu/pocketsphinx/*.java && jar -cf pocketsphinx.jar edu libpocketsphinx_jni.so
+
+if [ $? -eq 1 ]
+then
+  echo "Could not build pocketsphinx for java"
+  exit 1
+else
+  echo "pocketsphinx for java was successfully built" >> setup.log
+fi
 
 cp pocketsphinx.jar ../../../core/libs
 
@@ -70,10 +89,15 @@ rm cmuclmtk-0.7.tar.gz
 mv cmuclmtk-0.7 cmuclmtk
 
 cd cmuclmtk
-./configure --prefix=$PWD/installation
 
-make
-make check
-make install
+./configure --prefix=${PWD}/installation && make && make check && make install
+
+if [ $? -eq 1 ]
+then
+  echo "Could not build cmuclmtk"
+  exit 1
+else
+  echo "cmuclmtk was successfully built and installed" >> setup.log
+fi
 
 cd ..
