25b21e0ce8209c987e8155d57e866a369648bdab
Georgios Ouzounis <gouzouni625@gmail.com>
2016-09-29 21:28:30 +0300

    Add documentation for PrettyFormatter

--- src/main/java/org/pasr/utilities/logging/PrettyFormatter.java
+++ src/main/java/org/pasr/utilities/logging/PrettyFormatter.java
@@ -1,27 +1,39 @@
 package org.pasr.utilities.logging;
 
-
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
 
+/**
+ * @class PrettyFormatter
+ * @brief Log Formatter
+ */
 public class PrettyFormatter extends SimpleFormatter {
+
+    /**
+     * @brief Returns the give LogRecord message formatted
+     *
+     * @param record
+     *     The LogRecord the message of which to format
+     *
+     * @return The formatted LogRecord message
+     */
     @Override
-    public synchronized String format(LogRecord record) {
+    public synchronized String format (LogRecord record) {
         Level level = record.getLevel();
         String message = record.getMessage();
 
-        if(level == Level.SEVERE || level == Level.FINEST){
+        if (level == Level.SEVERE || level == Level.FINEST) {
             message = message.replaceAll("\n", "\n        ");
         }
-        else if(level == Level.WARNING){
+        else if (level == Level.WARNING) {
             message = message.replaceAll("\n", "\n         ");
         }
-        else if(level == Level.INFO || level == Level.FINE){
+        else if (level == Level.INFO || level == Level.FINE) {
             message = message.replaceAll("\n", "\n      ");
         }
-        else if(level == Level.FINER){
+        else if (level == Level.FINER) {
             message = message.replaceAll("\n", "\n       ");
         }
 
